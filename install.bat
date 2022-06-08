cfacb2d8432518fa3cd90f70ed0b6dbc68690bf0
000 <65537922+kntjspr@users.noreply.github.com>
2022-06-09 03:33:53 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,17 @@
 @echo off
 cd /d %~dp0
 setup.exe /configure config.xml
-REG ADD HKCU\Software\Policies\Microsoft\Office\16.0\officeupdate /v hideenabledisableupdates /t REG_DWORD /d 1
+echo Installation finished. Trying to execute product activation script.
+pause
+cd /d %ProgramFiles(x86)%\Microsoft Office\Office16
+cd /d %ProgramFiles%\Microsoft Office\Office16
+for /f %x in ('dir /b ..\root\Licenses16\proplusvl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
+cscript ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99
+cscript ospp.vbs /unpkey:BTDRB >nul
+cscript ospp.vbs /unpkey:KHGM9 >nul
+cscript ospp.vbs /unpkey:CPQVG >nul
+cscript ospp.vbs /sethst:s8.uk.to
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /act
+echo Product activation script executed.
 pause
