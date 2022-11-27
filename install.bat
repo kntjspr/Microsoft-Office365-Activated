b4ded760adcc849d174735132b8a619e39308f3d
kntjsprr <65537922+kntjsprr@users.noreply.github.com>
2022-11-27 17:50:21 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,6 +6,7 @@ pause
 cd /d %ProgramFiles(x86)%\Microsoft Office\Office16
 cd /d %ProgramFiles%\Microsoft Office\Office16
 for /f %x in ('dir /b ..\root\Licenses16\proplusvl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
+pause
 cscript ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99
 cscript ospp.vbs /unpkey:BTDRB >nul
 cscript ospp.vbs /unpkey:KHGM9 >nul
